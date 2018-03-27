01124d4e2f8be94c7690daa4faba2b28d62913bd
Gustavo Moura <gustavolpmoura@gmail.com>
2018-03-27 15:26:29 -0300

    ColorGame finalizado
    Arquivos atualizados com o código completo do jogo ColorGame

--- colorGame.js
+++ colorGame.js
@@ -6,55 +6,86 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
-var easyBtn = document.querySelector("#easyBtn");
-var hardBtn = document.querySelector("#hardBtn");
+//var easyBtn = document.querySelector("#easyBtn");
+//var hardBtn = document.querySelector("#hardBtn");
+var modeButtons = document.querySelectorAll(".mode");
 
-//easy button
-easyBtn.addEventListener("click", function(){
-	hardBtn.classList.remove("selected");
-	easyBtn.classList.add("selected");
-	numSquares = 3;
-	colors = generateRandomColors(numSquares);
-	pickedColor = pickColor();
-	colorDisplay.textContent = pickedColor
-	for(var i =0; i < squares.length; i++){
-		if(colors[i]){
-			squares[i].style.backgroundColor = colors[i];
-		} else{
-			squares[i].style.display = "none";
-		}
+init();
+
+function init(){
+	//mode buttons event listeners
+	for (var i = 0; i < modeButtons.length; i++) {
+		modeButtons[i].addEventListener("click", function function_name(argument) {
+			modeButtons[0].classList.remove("selected");
+			modeButtons[1].classList.remove("selected");
+			this.classList.add("selected");
+			this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+			reset();
+		});
 	}
+}
 
-});
-
-//hard button
-hardBtn.addEventListener("click", function(){
-	easyBtn.classList.remove("selected");
-	hardBtn.classList.add("selected");
-	numSquares = 6;
-	colors = generateRandomColors(numSquares);
-	pickedColor = pickColor();
-	colorDisplay.textContent = pickedColor
-	for(var i =0; i < squares.length; i++){
-			squares[i].style.backgroundColor = colors[i];
-			squares[i].style.display = "block";
-	}
-});
-
-//reset button
-resetButton.addEventListener("click", function(){
+function reset(){
 	//generate all new colors
 	colors = generateRandomColors(numSquares);
 	//pick a new random color fro array
 	pickedColor = pickColor();
 	//change colorDisplay to match picked color
 	colorDisplay.textContent = pickedColor;
+	resetButton.textContent = "New Colors";
+
+	//hide messageDisplay
+	messageDisplay.textContent = "";
 
 	//change colors of square
 	for(var i =0; i< squares.length; i++){
+		if(colors[i]){
+			squares[i].style.display = "block";
+			squares[i].style.backgroundColor = colors[i];
+		} else{
+			squares[i].style.display = "none";
+		}
 		squares[i].style.backgroundColor = colors[i];
 	}
-	h1.style.backgroundColor = "#232323";
+	h1.style.backgroundColor = "steelblue";
+
+}
+
+//easy button
+// easyBtn.addEventListener("click", function(){
+// 	hardBtn.classList.remove("selected");
+// 	easyBtn.classList.add("selected");
+// 	numSquares = 3;
+// 	colors = generateRandomColors(numSquares);
+// 	pickedColor = pickColor();
+// 	colorDisplay.textContent = pickedColor
+// 	for(var i =0; i < squares.length; i++){
+// 		if(colors[i]){
+// 			squares[i].style.backgroundColor = colors[i];
+// 		} else{
+// 			squares[i].style.display = "none";
+// 		}
+// 	}
+
+// });
+
+// //hard button
+// hardBtn.addEventListener("click", function(){
+// 	easyBtn.classList.remove("selected");
+// 	hardBtn.classList.add("selected");
+// 	numSquares = 6;
+// 	colors = generateRandomColors(numSquares);
+// 	pickedColor = pickColor();
+// 	colorDisplay.textContent = pickedColor
+// 	for(var i =0; i < squares.length; i++){
+// 			squares[i].style.backgroundColor = colors[i];
+// 			squares[i].style.display = "block";
+// 	}
+// });
+
+//reset button
+resetButton.addEventListener("click", function(){
+	reset();
 })
 
 colorDisplay.textContent = pickedColor;
@@ -119,3 +150,4 @@ function randomColor(){
 	
 	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
+
